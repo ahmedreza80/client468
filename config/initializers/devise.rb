fde525cd0968dbe0b1979ad19898d077070dfafa
ahmedreza80 <mdahmed@shiftind.com>
2019-06-03 17:51:13 +0530

    djfhdjjvhfs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,7 +262,7 @@ config.secret_key = 'd73ac1f858fe4a7ac7d11fbaa1b40a5c7d7adfe5caad8b607895bb13d95
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
