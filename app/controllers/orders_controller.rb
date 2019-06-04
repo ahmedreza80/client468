3c2c2b53341f7df5bd3d0b8a4fd2c26fb2959c39
ahmedreza80 <mdahmed@shiftind.com>
2019-06-04 05:45:26 +0530

    sdsdddsjssssssd

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
+
+	def order_tracking
+	    if params[:order_number].present?
+	      if Order.where(id: params[:order_number]).exists?
+	        @order = Order.find(params[:order_number])
+	        redirect_to  action: 'order_status', order_number: @order.id
+	      else
+	      	flash[:success] = "Please enter correct tracking Number"
+	      end	
+	    end  		
+	end
+
+	def order_status
+		if params[:order_number].present?
+	    	@order = Order.find(params[:order_number])
+		end
+	end
 	def new
 		@order = current_cart.order
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,14 @@ end
 
   resources :charges
 
-  get '/cart', to: 'order_items#index'
+
+get '/cart', to: 'order_items#index'
   resources :order_items, path: '/cart/items'
   get '/cart/checkout', to: 'orders#new', as: :checkout
   patch '/cart/checkout', to: 'orders#create'
   get '/cart/tracking', to: 'orders#show', as: :tracking
-
+  get '/order_tracking', to: 'orders#order_tracking'
+  get '/order_status', to: 'orders#order_status'
 
 
   
