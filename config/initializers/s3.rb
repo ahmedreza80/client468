e26078536bfd5bfbdb2fae52a605848b1fc8ba5a
Ahmed Reza <ahmedreza80@gmail.com>
2019-06-15 18:14:41 +0530

    kdhfsjh

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -4,9 +4,9 @@ require 'fog'
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAQZ6XZSKKX5NED2U6",
-      :aws_secret_access_key  => "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",
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+      :aws_access_key_id      => "AKIAJKMBIDHAHKX6MHNQ",
+      :aws_secret_access_key  => "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 ",
+      :region                 => 'ap-south-1' # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = "theddbucket"
+  config.fog_directory  = "nearclickbucket"
 end
