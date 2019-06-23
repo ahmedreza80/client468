5a238e45135aa89181ef1f44e46ae7947f991848
Ahmed Reza <ahmedreza80@gmail.com>
2019-06-23 13:57:04 +0530

    dhfssdssds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,28 @@
 class WelcomeController < ApplicationController
   respond_to :html, :xml, :json
+
+
+
+
+
   def home
+    # I've just copy-paste your code with database querying
+    # It should be optimized with scopes at model level,
+    # but this is not the main question
     @highlights = Highlight.all
+    @products = Product.where('(name ILIKE ? OR description ILIKE ? )', "%#{params[:search]}%", "%#{params[:search]}%")
+    @products = @products.where(state: params[:state]) if params[:state].present?
+    @products = @products.where(city: params[:city]) if params[:city].present?
+
+    if current_user
+      current_user.update_attributes(
+        search_state: params[:state],
+        search_city: params[:city],
+        search_area: params[:area]
+      )
+    end
+
+    respond_with @products
   end
   def fap
     @products = Product.all
