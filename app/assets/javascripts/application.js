23782983a304b781e3e640c3dcf55e25305fa721
Ahmed Reza <ahmedreza80@gmail.com>
2019-06-25 00:40:15 +0530

    fjdfhjhdddsdsh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+$(document).on('turbolinks:load', function(){
+  $('.flexslider').flexslider({
+    animation: "fade",
+    slideshowSpeed: 4000,
+    animationSpeed: 600,
+    controlNav: false,
+    directionNav: true,
+    controlsContainer: ".flex-container" // the container that holds the flexslider
+  });
+});
